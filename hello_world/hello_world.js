328023563817b201d8e79bdd0e31e6341a4a1197
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-04-30 20:19:38 +0800

    hello world

--- hello_world/hello_world.js
+++ hello_world/hello_world.js
@@ -8,6 +8,7 @@ let newApp = Vue.createApp({
   }
 })
 
-// 将应用与页面关联起来，这里将 id 为 app 的元素挂载到一个 Vue 实例上
-let vm = newApp.mount('#app')
+// mount 的返回值是一个 vm 实例，可以用来操作这个实例，比如设置数据、添加事件响应器等。
+// 在这个例子中，我们将返回的 vm 实例挂载到 id 为 app 的元素上。也就是将 Vue 的一个应用实例挂载到 DOM 元素上。
 // 此时，我们在浏览器的 Console 中，使用 vm.message="Hello Vue!" 以改变 message 的值
+let vm = newApp.mount('#app')
