40b21eeb82a20444f2c45677e11ce6c4bf7f85b1
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-04-30 18:23:55 +0800

    Update hello_world.js

--- hello_world/hello_world.js
+++ hello_world/hello_world.js
@@ -1,8 +1,12 @@
-Vue.createApp({
+// 创建一个新的应用实例
+let newApp = Vue.createApp({
   data() {
     return {
       message: 'Hello Vue!',
       myname: "Lch"
     }
   }
-}).mount('#app')
+})
+
+// 将应用与页面关联起来，这里将 id 为 app 的元素挂载到一个 Vue 实例上
+newApp.mount('#app')
