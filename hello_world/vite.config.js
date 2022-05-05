4f9c2ec79b043ded2c568ea65a3fc360a748b8e2
DesistDaydream <373406000@qq.com>
2022-05-05 11:24:16 +0800

    Update vite.config.js

--- hello_world/vite.config.js
+++ hello_world/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // [Vue warn]: Component provided template option but runtime compilation is not supported in this build of Vue. Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
 // 通过 vite 的配置设置别名，可以避免此报错，若不使用 vite 别名，则需要在 js 代码中，使用 import * as Vue from 'vue/dist/vue.esm-bundler.js' 导入 vue
 export default defineConfig({
-    plugins: [],
     resolve: {
       alias: {
         'vue': 'vue/dist/vue.esm-bundler.js'
