ed0248a8b26ae34f041809e6529acbcf535d99bb
DesistDaydream <373406000@qq.com>
2022-10-26 12:18:10 +0800

    改用组合式 API

--- my_first_vue/src/components/2_Template.vue
+++ my_first_vue/src/components/2_Template.vue
@@ -1,3 +1,19 @@
+<script setup>
+// 文本插值
+let msg = "文本插值"
+// 原始 HTML
+let rawHtml = "<a href='https://github.com/DesistDaydream'>DesistDaydream</a>"
+// 属性绑定
+let dynamicId = 102205
+// 使用 JavaScript 表达式
+let number = 10
+let ok = true
+let message = "Hello World"
+let id = "DesistDaydream"
+// 指令
+
+</script>
+
 <!-- 官方文档，模板语法：https://cn.vuejs.org/guide/essentials/template-syntax.html -->
 <template>
     <!-- 1. Text interpolation(文本插值) -->
@@ -7,46 +23,23 @@
 
     <!-- 2. 原始 HTML -->
     <!-- 双大括号会将数据解释为纯文本，而不是 HTML。若想插入 HTML，需要使用 v-html 指令： -->
-    <p>使用文本插值，插入 HTMl: {{ rawHtml }}</p>
-    <p>使用 v-html 指令，插入 HTMl: <span v-html="rawHtml"></span></p>
+    <p>插入 HTML(使用文本插值): {{ rawHtml }}</p>
+    <p>插入 HTML(使用 v-html 指令): <span v-html="rawHtml"></span></p>
     <!-- ！！！注意：在网站上动态渲染任意 HTML 是非常危险的，因为这非常容易造成 XSS 漏洞。请仅在内容安全可信时再使用 v-html，并且永远不要使用用户提供的 HTML 内容。 -->
 
     <!-- 3. Attribute(属性) 绑定 -->
     <!-- 双大括号无法应用在“标签的属性”中，如果想要只渲染属性，则使用 v-bind 指令： -->
-    <div v-bind:id="dynamicId"></div>
+    <div v-bind:id="dynamicId">查看该元素标签的属性，以查看属性绑定示例</div>
     <!-- v-bind 可以省略，只使用一个冒号即可 -->
 
     <!-- 4. 使用 JavaScript 表达式 -->
     <!-- 可以在双大括号中使用 JavaScript 表达式。注意：仅支持“单一表达式”，不要使用复杂的 -->
-    {{ number + 1 }}
-    {{ ok ? 'ok变量值为 true' : 'ok 变量值为 false ' }}
-    {{ message.split('').reverse().join('') }}
-    <div :id="`list-${id}`"></div> <!-- 简写的 v-bind 指令 -->
+    <p>JavaScript 表达式: {{ number + 1 }}</p>
+    <p>JavaScript 表达式: {{ ok ? 'ok变量值为 true' : 'ok 变量值为 false ' }}</p>
+    <p>JavaScript 表达式: {{ message.split('').reverse().join('') }}</p>
+    <p :id="`list-${id}`">JavaScript 表达式: 在标签的属性中查看示例效果</p> <!-- 简写的 v-bind 指令 -->
 
     <!-- 5. Directives(指令) -->
     <!-- 指令是带有 v- 前缀的特殊 attribute。Vue 提供了许多内置指令，包括上面我们所介绍的 v-bind 和 v-html。 -->
 
 </template>
-
-<script>
-let needInter = "文本插值"
-export default {
-    data() {
-        return {
-            // 文本插值
-            msg: needInter,
-            // 原始 HTML
-            rawHtml: "<a href='https://github.com/DesistDaydream'>DesistDaydream</a>",
-            // 属性绑定
-            dynamicId: 102205,
-            // 使用 JavaScript 表达式
-            number: 10,
-            ok: true,
-            message: "Hello World",
-            id: "DD"
-            // 指令
-        }
-    }
-}
-</script>
-
