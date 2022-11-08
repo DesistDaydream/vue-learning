0fd4ed1dddc80b4afcf9f9c60ddde6c30e27133e
DesistDaydream <373406000@qq.com>
2022-11-08 17:29:27 +0800

    组件props传递，子传父

--- my_first_ts_vue/src/components/3_components/Components.vue
+++ my_first_ts_vue/src/components/3_components/Components.vue
@@ -4,7 +4,7 @@ import ComponentTwo from "@/components/3_components/ComponentTwo.vue"
 import ComponentThree from "@/components/3_components/ComponentThree.vue"
 import PassingPropsDemo from "@/components/3_components/PassingPropsDemo.vue"
 import PassingProps from "@/components/3_components/PassingProps.vue"
-import ListeningEvents from "@/components/3_components/ListeningEvents.vue"
+import PassingPropsListeningEvents from "@/components/3_components/PassingPropsListeningEvents.vue"
 
 const Titles = ref([
   { id: 1, title: "我的 Vue 之旅" },
@@ -17,29 +17,41 @@ const titlesFontSize = ref(1)
 
 <template>
   <h1>组件的基本调用</h1>
-  <!-- 调用第二个组件 -->
-  <ComponentTwo />
-  <!-- 调用第三个组件 -->
-  <ComponentThree />
-  <!-- 同一个组件可以重复调用 -->
-  <ComponentTwo />
-  <!-- 可以调用多个组件以拼出来整个页面 -->
+  <div>
+    <!-- 调用第二个组件 -->
+    <ComponentTwo />
+    <!-- 调用第三个组件 -->
+    <ComponentThree />
+    <!-- 同一个组件可以重复调用 -->
+    <ComponentTwo />
+    <!-- 可以调用多个组件以拼出来整个页面 -->
+  </div>
 
   <h1>组件调用时传递 Props</h1>
   <!-- 传递 Props -->
-  <PassingPropsDemo
-    title="组件之间的交互传递的数据"
-    msg="组件之间的交互传递的数据"
-  />
+  <div>
+    <PassingPropsDemo
+      title="组件之间的交互传递的数据"
+      msg="组件之间的交互传递的数据"
+    />
+  </div>
   <!-- 通过 v-for 指令多次调用组件，且多次传递 props -->
   <PassingProps v-for="t in Titles" :key="t.id" :title="t.title"></PassingProps>
-  <!-- 监听事件 -->
-  <div :style="{ fontSize: titlesFontSize + 'em' }">
-    <ListeningEvents
-      msg="这次字体将会随着按钮点击而变化"
-      @enlarge-text="titlesFontSize += 0.1"
-      @reduce-text="titlesFontSize -= 0.1"
-    />
+
+  <h2>props 的子组件到父组件传递</h2>
+  <div>
+    <p>
+      我们可以通过 defineEmits() 函数创建监听事件以便让 props
+      从子组件传递到父组件
+    </p>
+    <!-- 监听事件 -->
+    <div :style="{ fontSize: titlesFontSize + 'em' }">
+      <PassingPropsListeningEvents
+        msg="这次字体将会随着按钮点击而变化"
+        @enlarge-text="titlesFontSize += 0.1"
+        @reduce-text="titlesFontSize -= 0.1"
+      />
+    </div>
   </div>
 </template>
 
---
+++ my_first_ts_vue/src/components/3_components/PassingPropsListeningEvents.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+
+// 声明触发的事件
+const emit = defineEmits<{
+  (event: "enlarge-text"): void
+  (event: "reduce-text"): void
+}>()
+
+// 点击事件
+const enlargeText = () => {
+  emit("enlarge-text")
+}
+const reduceText = () => {
+  emit("reduce-text")
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <button @click="enlargeText">放大字体</button>
+  <button @click="reduceText">缩小字体</button>
+</template>
+
+<style></style>
